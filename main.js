67a0ab2da5f93b3a7af17790b380670ac81898ce
mradulsoni5 <mradulsoni1901@gmail.com>
2024-10-19 16:42:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text",{
+    strings:["Frontend Developer","Web Developer","Machine Learner"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDisplay:1000,
+    loop:true
+});
